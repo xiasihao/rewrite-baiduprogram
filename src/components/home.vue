1bc02a7c5db573d6ecb8c16f70a9ae6db39c7ba8
xiasihao <18372861004@163.com>
2020-08-05 10:39:34 +0800

    first time commit

---
+++ src/components/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>{{msg}}</div>
+    <Nav dataA="nav" />
+  </div>
+</template>
+
+<script>
+import Nav from './nav'
+export default {
+  name: 'home',
+  components: {
+    Nav
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/nav.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        nav
+        {{dataA}}
+    </div>
+</template>
+<script>
+export default {
+    name: 'nav',
+    props: {
+        dataA: String
+    }
+}
+</script>
+<style>
+
+</style>
